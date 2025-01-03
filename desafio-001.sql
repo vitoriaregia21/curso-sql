fdf596161c9517b26121f42c3a5469dc3bba9a64
Vitoria <vitoriasilva8316@gmail.com>
2025-01-03 01:02:57 -0300

    desafio-001-sql

---
+++ desafio-001.sql
@@ -0,0 +1,29 @@
+-- EXERCÍCIOS ######################################################################
+
+-- (Exercício 1) Selecione os nomes de cidade distintas que existem no estado de
+-- Minas Gerais em ordem alfabética (dados da tabela sales.customers)
+	select state, city
+	from sales.customers
+	where state = 'MG'
+	order by city
+
+-- (Exercício 2) Selecione o visit_id das 10 compras mais recentes efetuadas
+-- (dados da tabela sales.funnel)
+	select visit_id
+	from sales.funnel
+	order by finish_checkout_date
+	limit 10
+
+-- (Exercício 3) Selecione todos os dados dos 10 clientes com maior score nascidos
+-- após 01/01/2000 (dados da tabela sales.customers)
+	select *
+	from sales.customers 
+	where birth_date > '20000101'
+	order by score desc
+	limit 10
+
+
+
+
+
+	
